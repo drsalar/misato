449766a2385e17f72cf736ef1b22e28c2e9a69b1
lijiadong <lijiadong@bilibili.com>
2017-10-10 17:58:57 +0800

    b

--- main.go
+++ main.go
@@ -15,16 +15,11 @@ import (
 )
 
 type Conf struct {
-	Pos       Position `json:"position"`
-	Siz       Size     `json:"size"`
-	Mode      int      `json:"mode"`
-	Alpha     int      `json:"alpha"`
-	Watermark string   `json:"watermark"`
-}
-
-type Position struct {
-	X int `json:"x"`
-	Y int `json:"y"`
+	Margin    Size   `json:"margin"`
+	Siz       Size   `json:"size"`
+	Mode      int    `json:"mode"`
+	Alpha     int    `json:"alpha"`
+	Watermark string `json:"watermark"`
 }
 
 type Size struct {
@@ -46,7 +41,6 @@ func main() {
 	}
 
 	filesWalker()
-	test()
 }
 
 func filesWalker() {
@@ -66,26 +60,28 @@ func run(path string, f os.FileInfo, err error) error {
 
 	fmt.Println("open file:", path)
 
-	if filepath.Ext(path) == ".jpg" {
-		jpgHandler(path)
-	} else if filepath.Ext(path) == ".png" {
-		// pngHandler(path)
-	}
+	imgHandler(path)
 
 	return nil
 }
 
-func jpgHandler(path string) {
+func imgHandler(path string) {
 	rmgb, _ := os.Open(path)
-	rimg, _ := jpeg.Decode(rmgb)
+
+	var rimg image.Image
+	if filepath.Ext(path) == ".jpg" {
+		rimg, _ = jpeg.Decode(rmgb)
+	} else if filepath.Ext(path) == ".png" {
+		rimg, _ = png.Decode(rmgb)
+	}
 	defer rmgb.Close()
 
 	wmb, _ := os.Open(conf.Watermark)
 	watermark, _ := png.Decode(wmb)
 	defer wmb.Close()
 
-	//把水印写到右下角，并向0坐标各偏移10个像素
-	offset := image.Pt(rimg.Bounds().Dx()-watermark.Bounds().Dx()-10, rimg.Bounds().Dy()-watermark.Bounds().Dy()-10)
+	// offset := image.Pt(rimg.Bounds().Dx()-watermark.Bounds().Dx()-10, rimg.Bounds().Dy()-watermark.Bounds().Dy()-10)
+	offset := calOffect(rimg.Bounds().Dx(), rimg.Bounds().Dy(), watermark.Bounds().Dx(), watermark.Bounds().Dy())
 	b := rimg.Bounds()
 	m := image.NewNRGBA(b)
 
@@ -97,16 +93,39 @@ func jpgHandler(path string) {
 	}
 
 	draw.Draw(m, b, rimg, image.ZP, draw.Src)
-	// draw.Draw(m, watermark.Bounds().Add(offset), watermark, image.ZP, draw.Over)
 	draw.DrawMask(m, watermark.Bounds().Add(offset), watermark, image.ZP, yy, image.ZP, draw.Over)
 
-	//生成新图片new.jpg，并设置图片质量..
 	imgw, _ := os.Create(dir + "/new/" + filepath.Base(path))
 	jpeg.Encode(imgw, m, &jpeg.Options{100})
 
 	defer imgw.Close()
 
 	fmt.Println("水印添加结束,请查看new.jpg图片...")
+}
+
+func calOffect(x1, y1 int, x2, y2 int) (pt image.Point) {
+	switch conf.Mode {
+	case TOP_LEFT:
+		pt = image.Pt(conf.Margin.W, conf.Margin.H)
+		return
+	case BOTTOM_RIGHT:
+		pt = image.Pt(x1-x2-conf.Margin.W, y1-y2-conf.Margin.H)
+		return
+	case TOP_CENTER:
+		pt = image.Pt((x1-x2)/2, conf.Margin.H)
+		return
+	case BOTTOM_CENTER:
+		pt = image.Pt((x1-x2)/2, y1-y2-conf.Margin.H)
+		return
+	case TILE:
+		//TODO
+	case STRETCH:
+		//TODO
+	default:
+		//TODO
+	}
+	pt = image.Pt(conf.Margin.W, conf.Margin.H)
+	return
 }
 
 func confInit() {
@@ -119,31 +138,4 @@ func confInit() {
 	if err != nil {
 		log.Fatal("error in confInit:", err.Error())
 	}
-}
-
-func test() {
-	x, _ := os.Open("xxxx.png")
-	xx, _ := png.Decode(x)
-	defer x.Close()
-
-	d, _ := os.Open("tttt.jpg")
-	dd, _ := jpeg.Decode(d)
-	defer d.Close()
-
-	offset := image.Pt(dd.Bounds().Dx()-xx.Bounds().Dx()-10, dd.Bounds().Dy()-xx.Bounds().Dy()-10)
-
-	mm := image.NewRGBA(dd.Bounds())
-	yy := image.NewRGBA(image.Rect(0, 0, xx.Bounds().Dx(), xx.Bounds().Dy()))
-	for x := 0; x < yy.Bounds().Dx(); x++ {
-		for y := 0; y < yy.Bounds().Dy(); y++ {
-			yy.SetRGBA(x, y, color.RGBA{255, 255, 255, uint8(conf.Alpha)})
-		}
-	}
-
-	draw.Draw(mm, dd.Bounds(), dd, image.ZP, draw.Src)
-	// draw.Draw(mm, xx.Bounds().Add(offset), xx, image.ZP, draw.Over)
-	draw.DrawMask(mm, xx.Bounds().Add(offset), xx, image.ZP, yy, image.ZP, draw.Over)
-
-	p, _ := os.Create("ssss.png")
-	png.Encode(p, mm)
 }
